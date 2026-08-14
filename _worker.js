032bc7dc9eba61e249952fa1a4352560544f94d5
noparsa21 <noparsa21@gmail.com>
2026-08-14 14:39:11 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 /*
- * Project Nahan (نهان) - IoT Device Telemetry Gateway
+ * Project Nahan (noparsa) - IoT Device Telemetry Gateway
  * Handles real-time binary streams from remote sensor nodes.
  */
 
